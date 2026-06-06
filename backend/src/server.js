e217c4e140b7dec992a4aeacef741babd0b02e1e
Oops Prem <oopsprem05@gmail.com>
2026-06-07 02:48:19 +0530

    Fix Render production routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,10 +10,9 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-console.log("NODE_ENV =", process.env.NODE_ENV);
+const PORT = process.env.PORT || 5001;
 
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({ origin: "http://localhost:5173" }));
@@ -25,9 +24,8 @@ app.use("/api/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
